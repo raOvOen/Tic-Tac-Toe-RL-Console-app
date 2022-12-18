4588198c45cc5532302307150d06aa6c75d1f23b
raOvOen <san2012ches@gmail.com>
2022-12-19 00:15:11 +0300

    major update

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import random
 import os
 import time
+from contextlib import redirect_stdout
+import matplotlib.pyplot as plt
 
 def clear_cmd():
     os.system('cls')
 
 def check_if_end(game_field):
-    for i in range(1,3):
-        if(((game_field[0] == i) and (game_field[1] == i) and (game_field[2] == i)) or ((game_field[3] == i) and (game_field[4] == i) and (game_field[5] == i)) or ((game_field[6] == i) and (game_field[7] == i) and (game_field[8] == i))):
-            return True, i
-        elif(((game_field[0] == i) and (game_field[3] == i) and (game_field[6] == i)) or ((game_field[1] == i) and (game_field[4] == i) and (game_field[7] == i)) or ((game_field[2] == i) and (game_field[5] == i) and (game_field[8] == i))):
-            return True, i
-        elif(((game_field[0] == i) and (game_field[4] == i) and (game_field[8] == i)) or ((game_field[2] == i) and (game_field[4] == i) and (game_field[6] == i))):
-            return True, i
-    return False, i
+    for winner_number in range(1,3):
+        if(((game_field[0] == winner_number) and (game_field[1] == winner_number) and (game_field[2] == winner_number)) or ((game_field[3] == winner_number) and (game_field[4] == winner_number) and (game_field[5] == winner_number)) or ((game_field[6] == winner_number) and (game_field[7] == winner_number) and (game_field[8] == winner_number))):
+            return True, winner_number
+        elif(((game_field[0] == winner_number) and (game_field[3] == winner_number) and (game_field[6] == winner_number)) or ((game_field[1] == winner_number) and (game_field[4] == winner_number) and (game_field[7] == winner_number)) or ((game_field[2] == winner_number) and (game_field[5] == winner_number) and (game_field[8] == winner_number))):
+            return True, winner_number
+        elif(((game_field[0] == winner_number) and (game_field[4] == winner_number) and (game_field[8] == winner_number)) or ((game_field[2] == winner_number) and (game_field[4] == winner_number) and (game_field[6] == winner_number))):
+            return True, winner_number
+    if (game_field.count(0) == 0):
+        return True, 0
+    return False, winner_number
 
 def int_list_to_str(game_field):
     ret_str = ""
@@ -68,7 +72,7 @@ def print_board(game_field):
     print(" ---------------------")
     print("\n")
 
-def AI_set_mark(mark_to_set, game_field, empty_index_list, moves_history, need_to_explore):
+def AI_set_mark(game_field, empty_index_list, need_to_explore):
     cur_key_state = int_list_to_str(game_field)
     if (cur_key_state not in AI_exp.keys()):
         cur_key_values = [-1.0] * 9
@@ -84,12 +88,9 @@ def AI_set_mark(mark_to_set, game_field, empty_index_list, moves_history, need_t
             put_mark_index = random.choice(empty_index_list)
         else:
             put_mark_index = cur_value_state.index(max(cur_value_state))
-    game_field[put_mark_index] = mark_to_set
-    empty_index_list.remove(put_mark_index)
-    moves_history[cur_key_state] = put_mark_index
-    return game_field, empty_index_list, moves_history
+    return cur_key_state, put_mark_index
 
-def player_set_mark(mark_to_set, game_field, empty_index_list, moves_history):
+def player_set_mark(game_field, empty_index_list):
     cur_key_state = int_list_to_str(game_field)
     if (cur_key_state not in AI_exp.keys()):
         cur_key_values = [-1.0] * 9
@@ -105,112 +106,108 @@ def player_set_mark(mark_to_set, game_field, empty_index_list, moves_history):
         else:
             break
         clear_cmd()
-    game_field[put_mark_index] = mark_to_set
-    empty_index_list.remove(put_mark_index)
-    moves_history[cur_key_state] = put_mark_index
-    return game_field, empty_index_list, moves_history
+    return cur_key_state, put_mark_index
 
-def get_more_experience(moves_history, give_reward):
-    cur_value_state = AI_exp[list(moves_history.keys())[-1]]
-    cur_value_state[list(moves_history.values())[-1]] = give_reward
-    moves_amount = len(moves_history.keys())
-    for i in range(1, moves_amount + 1):
-        cur_value_state = AI_exp[list(moves_history.keys())[moves_amount - i]]
-        V_s = cur_value_state[list(moves_history.values())[moves_amount - i]]
-        V_s = V_s + alpha * (give_reward - V_s)
-        give_reward = V_s
-        cur_value_state[list(moves_history.values())[moves_amount - i]] = V_s
-        AI_exp[list(moves_history.keys())[moves_amount - i]] = cur_value_state
+def Simple_TicTacToe_algorithm(game_field, empty_index_list, mark_to_set):
+    cur_key_state = int_list_to_str(game_field)
+    if (cur_key_state not in AI_exp.keys()):
+        cur_key_values = [-1.0] * 9
+        for i in empty_index_list:
+            cur_key_values[i] = 0.5
+        AI_exp[cur_key_state] = cur_key_values
+    for cur_index in empty_index_list:
+        possible_game_field = game_field.copy()
+        possible_game_field[cur_index] = mark_to_set
+        if(check_if_end(possible_game_field)[0] == True):
+            return cur_key_state, cur_index
+    put_mark_index = random.choice(empty_index_list)
+    return cur_key_state, put_mark_index
+
+def play_game(play_game_type, start_first):
+    game_field = [0] * 9
+    empty_index_list = list(range(0, 9))
+    mark_move = 0
+    prev_put_mark_index_P2 = None
+    prev_key_state_P2 = None
+    prev_put_mark_index_P1 = None
+    prev_key_state_P1 = None
+    while (True):
+        end_game, win_numb = check_if_end(game_field)
+        if (end_game):
+            if (win_numb == 0):
+                give_reward = 0.5
+                get_move_experience(prev_key_state_P2, prev_put_mark_index_P2, give_reward)
+                break
+            get_move_experience(prev_key_state_P2, prev_put_mark_index_P2, 0.0)
+            AI_exp[prev_key_state_P1][prev_put_mark_index_P1] = 1.0
+            break
+        mark_move = (mark_move % 2) + 1
+        if(play_game_type==1):
+            cur_key_state, cur_put_mark_index = AI_set_mark(game_field, empty_index_list, True)
+        elif(play_game_type==2):
+            cur_key_state, cur_put_mark_index = player_set_mark(game_field, empty_index_list) if mark_move == start_first else AI_set_mark(game_field, empty_index_list, False)
+        elif(play_game_type==11):
+            cur_key_state, cur_put_mark_index = AI_set_mark(game_field, empty_index_list, True) if mark_move == start_first else Simple_TicTacToe_algorithm(game_field, empty_index_list, mark_move)
+        else:
+            cur_key_state, cur_put_mark_index = AI_set_mark(game_field, empty_index_list, True)
+        if (prev_put_mark_index_P2 != None):
+            cur_value_state = AI_exp[int_list_to_str(game_field)]
+            get_move_experience(prev_key_state_P2, prev_put_mark_index_P2, cur_value_state[cur_put_mark_index])
+        prev_put_mark_index_P2 = prev_put_mark_index_P1
+        prev_key_state_P2 = prev_key_state_P1
+        prev_put_mark_index_P1 = cur_put_mark_index
+        prev_key_state_P1 = cur_key_state
+        game_field[cur_put_mark_index] = mark_move
+        empty_index_list.remove(cur_put_mark_index)
+    return game_field, win_numb
+def get_move_experience(prev_key_state, prev_put_mark_index,reward):
+    prev_value_state = AI_exp[prev_key_state]
+    V_s = prev_value_state[prev_put_mark_index]
+    V_s = V_s + alpha * (reward - V_s)
+    prev_value_state[prev_put_mark_index] = V_s
+    AI_exp[prev_key_state] = prev_value_state
 
 def train_AI(need_to_train):
     print("\n[*] Starting hard AI training...")
     start_time = time.time()
     for temp_game in range(need_to_train):
-        game_field = [0]*9
-        empty_index_list = list(range(0, 9))
-        moves_history_P1 = {}
-        moves_history_P2 = {}
-        while(True):
-            game_field, empty_index_list, moves_history_P1 = AI_set_mark(1, game_field, empty_index_list, moves_history_P1, True)
-            if(check_if_end(game_field)[0] or game_field.count(0) == 0):
-                break
-            game_field, empty_index_list, moves_history_P2 = AI_set_mark(2, game_field, empty_index_list, moves_history_P2, True)
-            if(check_if_end(game_field)[0] or game_field.count(0) == 0):
-                break
-        end_game, win_numb = check_if_end(game_field)
-        #Give rewards for P1
-        if(end_game):
-            if(win_numb==1):
-                give_reward = 1.0
-            else:
-                give_reward = 0.0
-        else:
-            give_reward = 0.5
-        get_more_experience(moves_history_P1, give_reward)
-        # Give rewards for P2
-        if(end_game):
-            if(win_numb==2):
-                give_reward = 1.0
-            else:
-                give_reward = 0.0
-        else:
-            give_reward = 0.5
-        get_more_experience(moves_history_P2, give_reward)
+        play_game(1, None)
     print(f"[+] The hard training is over! It took: {time.time() - start_time} sec")
     input("[*] Press Enter to continue...")
 
 def play_with_AI(start_first):
-    global total_played_games
-    game_field = [0] * 9
-    empty_index_list = list(range(0, 9))
-    moves_history_Player = {}
-    moves_history_AI = {}
-    while(True):
-        if(start_first==1):
-            game_field, empty_index_list, moves_history_Player = player_set_mark(1, game_field, empty_index_list, moves_history_Player)
-            if (check_if_end(game_field)[0] or game_field.count(0) == 0):
-                break
-            game_field, empty_index_list, moves_history_AI = AI_set_mark(2, game_field, empty_index_list, moves_history_AI, False)
-            if (check_if_end(game_field)[0] or game_field.count(0) == 0):
-                break
-        else:
-            game_field, empty_index_list, moves_history_AI = AI_set_mark(1, game_field, empty_index_list, moves_history_AI, False)
-            if (check_if_end(game_field)[0] or game_field.count(0) == 0):
-                break
-            game_field, empty_index_list, moves_history_Player = player_set_mark(2, game_field, empty_index_list, moves_history_Player)
-            if (check_if_end(game_field)[0] or game_field.count(0) == 0):
-                break
-    end_game, win_numb = check_if_end(game_field)
-    # Give rewards for AI
-    if (end_game):
-        if ((win_numb == 1 and start_first == 2) or (win_numb == 2 and start_first == 1)):
-            give_reward = 1.0
-        else:
-            give_reward = 0.0
-    else:
-        give_reward = 0.5
-    get_more_experience(moves_history_AI, give_reward)
-    # Give rewards for Player
-    if (end_game):
-        if ((win_numb == 1 and start_first == 1) or (win_numb == 2 and start_first == 2)):
-            give_reward = 1.0
-        else:
-            give_reward = 0.0
-    else:
-        give_reward = 0.5
-    get_more_experience(moves_history_Player, give_reward)
+    game_field, win_numb = play_game(2, start_first)
     print_board(game_field)
-    if (end_game):
-        if ((win_numb == 1 and start_first == 2) or (win_numb == 2 and start_first == 1)):
-            print("[+] AI won!\nEasy peasy for such Strong AI!")
-            input("[*] Press Enter to continue...")
-        else:
-            print("[+] Player won!\nAI need to train harder!")
-            input("[*] Press Enter to continue...")
-    else:
+    if ((win_numb == 1 and start_first == 2) or (win_numb == 2 and start_first == 1)):
+        print("[+] AI won!\nEasy peasy for such Strong AI!")
+    elif(win_numb == 0):
         print("[+] Draw!\nAIt looks like a draw. I wonder if this game can be won?")
-        input("[*] Press Enter to continue...")
-    total_played_games += 1
+    else:
+        print("[+] Player won!\nAI need to train harder!")
+    input("[*] Press Enter to continue...")
+
+def draw_AI_progression(need_to_play, need_to_get_win_rate, start_first, opponent):
+    reset_exp()
+    x_cur_game_amount = []
+    y_cur_win_rate = []
+    print("\n[*] Starting hard AI training and drawing...")
+    start_time = time.time()
+    AI_won = 0
+    for temp_game in range(need_to_play):
+        game_field, win_numb = play_game(10 + opponent, start_first)
+        if ((win_numb == 1 and start_first == 1) or (win_numb == 2 and start_first == 2)):
+            AI_won += 1
+        if((temp_game%need_to_get_win_rate == 0) and (temp_game != 0)):
+            print(f"[*] AI won for {AI_won}/{temp_game} times! Current win rate is {(AI_won/temp_game) * 100}%")
+            x_cur_game_amount.append(temp_game)
+            y_cur_win_rate.append((AI_won/temp_game) * 100)
+    print(f"[+] The hard training is over! It took: {time.time() - start_time} sec")
+    print(f"[*] AI won for {AI_won}/{need_to_play} times!")
+    plt.plot(x_cur_game_amount, y_cur_win_rate)
+    plt.xlabel('Number of game parties')
+    plt.ylabel('Win rate')
+    plt.show()
+    input("[*] Press Enter to continue...")
 
 def reset_exp():
     global total_played_games
@@ -227,7 +224,7 @@ def menu():
         clear_cmd()
         print('[OvO] Tic Tac Toe Reinforcement Learning by raOvOen')
         print(f'\n[*] You current settings\n - Alpha (Learning rate): {alpha}\n - Epsilon (Random chance): {epsilon}\n - Total played games: {total_played_games}')
-        print(f'\n[*] Available options:\n1) Train AI\n2) Play with AI\n3) Change settings\n4) Show AI experience\n5) Reset AI experience\n6) Exit')
+        print(f'\n[*] Available options:\n1) Train AI\n2) Play with AI\n3) Draw AI progression graph (will reset AI experience)\n4) Change settings\n5) Print to txt AI experience\n6) Reset AI experience\n7) Exit')
         chosen_option = input("\n[?] Choose desire option: ")
         if(chosen_option=="1"):
             clear_cmd()
@@ -238,9 +235,22 @@ def menu():
         elif(chosen_option=="2"):
             clear_cmd()
             print("[?] Choose the move you will start with (1 or 2):")
-            start_first=int(input("\n[?] I want to start: "))
-            play_with_AI(start_first)
+            start_position=int(input("\n[?] I want to start: "))
+            play_with_AI(start_position)
+            total_played_games += 1
         elif(chosen_option=="3"):
+            clear_cmd()
+            print("[?] How many game AI need to play?:")
+            games_amount=int(input("\n[?] Choose desire amount: "))
+            print("[?] How often is it necessary to fix the win rate")
+            win_rate_freq_amount = int(input("\n[?] Choose game amount to get win rate: "))
+            print(f"[?] Who will be the AI's opponent?\n1) Simple Tic Tac Toe algorithm\n2) AI himself")
+            opponent = int(input("\n[?] Choose desire opponent number: "))
+            print("[?] Choose the move AI will start with (1 or 2):")
+            start_position = int(input("\n[?] AI will start: "))
+            draw_AI_progression(games_amount, win_rate_freq_amount, start_position,opponent)
+            total_played_games += games_amount
+        elif(chosen_option=="4"):
             clear_cmd()
             print(f"[?] What you want to change?\n1) Alpha (Learning rate): {alpha}\n2) Epsilon (Random chance): {epsilon}")
             option_to_change = int(input("\n[?] Choose desire option: "))
@@ -250,28 +260,31 @@ def menu():
                 epsilon = float(input("[?] Choose desire Epsilon (Random chance) amount (from 0.0 to 1.0, default - 0.3): "))
             print("[+] Successful parameter change!")
             input("[*] Press Enter to continue...")
-        elif(chosen_option=="4"):
+        elif(chosen_option=="5"):
             clear_cmd()
-            print("[*] Okay, lets start!")
-            for i in range(len(AI_exp.keys())):
-                print("-------------------------------------------------")
-                current_state = list(AI_exp.keys())[i]
-                print(f"\n[{i} state] {current_state}")
-                print_board(str_to_int_list(current_state))
-                print_values_like_board(AI_exp[current_state])
-                print("-------------------------------------------------")
             if(len(AI_exp.keys()) == 0):
+                open('AI_experience.txt', 'w').close()
                 print("[*] Hm... It looks like AI doesn't know how to play Tic-Tac-Toe! Necessary to teach him this.")
                 input("[*] Press Enter to continue...")
-            else:
-                print("\n[+] Oooh... That was hard enough.")
+                continue
+            print("[*] Saving AI progress to 'AI_experience.txt'...")
+            with open('AI_experience.txt', 'w') as f:
+                with redirect_stdout(f):
+                    for i in range(len(AI_exp.keys())):
+                        print("-------------------------------------------------")
+                        current_state = list(AI_exp.keys())[i]
+                        print(f"\n[{i} state] {current_state}")
+                        print_board(str_to_int_list(current_state))
+                        print_values_like_board(AI_exp[current_state])
+                        print("-------------------------------------------------")
+                print("[+] Oooh... That was hard enough.")
                 input("[*] Press Enter to continue...")
-        elif(chosen_option=="5"):
+        elif(chosen_option=="6"):
             clear_cmd()
             reset_exp()
             print("[+] Well, the AI has lost all its experience. So sad...")
             input("[*] Press Enter to continue...")
-        elif(chosen_option=="6"):
+        elif(chosen_option=="7"):
             break
         else:
             clear_cmd()
